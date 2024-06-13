18b5421543162ea34d8fa5483e54d64181ddc4a9
FrancoBalduini <134542513+FrancoBalduini@users.noreply.github.com>
2024-06-12 23:00:05 -0300

    guarderia services add

--- src/Application/Services/GuarderiaService.cs
+++ src/Application/Services/GuarderiaService.cs
@@ -14,7 +14,74 @@ namespace Application.Services
 {
     public class GuarderiaService : IGuarderiaService 
     {
-        
+         private readonly IGuarderiaRepository _guarderiaRepository;
+
+        public GuarderiaService(IGuarderiaRepository repository)
+        {
+            _guarderiaRepository = repository;
+        }
+
+        public List<GuarderiaDto> GetAll()
+        {
+            var list = _guarderiaRepository.GetAll();
+
+            return GuarderiaDto.CreateList(list);
+        }
+
+        public List<Guarderia> GetAllFullData()
+        {
+            return _guarderiaRepository.GetAll();
+        }
+
+        public GuarderiaDto GetById(int id)
+        {
+            var obj = _guarderiaRepository.GetById(id)
+            ?? throw new NotFoundException(nameof(Guarderia), id);
+
+
+            var dto = GuarderiaDto.Create(obj);
+
+            return dto;
+        }
+
+        public Guarderia Create(GuarderiaCreateRequest guarderiaCreateRequest)
+        {
+            var obj = new Guarderia();
+            obj.Nombre = guarderiaCreateRequest.Nombre;
+            obj.Capacidad = guarderiaCreateRequest.Capacidad;
+            
+            return _guarderiaRepository.Add(obj);
+        }
+
+        public void Update(int id, GuarderiaUpdateRequest guarderiaUpdateRequest)
+        {
+
+            var obj = _guarderiaRepository.GetById(id);
+
+            if (obj == null)
+                throw new NotFoundException(nameof(guarderia), id);
+
+            if (guarderiaUpdateRequest.Nombre != string.Empty) obj.Nombre = guarderiaUpdateRequest.Nombre;
+
+            if (guarderiaUpdateRequest.Capacidad != 0) obj.Capacidad = guarderiaUpdateRequest.Capacidad;
+
+            
+
+            _guarderiaRepository.Update(obj);
+
+        }
+
+        public void Delete(int id)
+        {
+            var obj = _guarderiaRepository.GetById(id);
+
+            if (obj == null)
+                throw new NotFoundException(nameof(Guarderia), id);
+
+            _guarderiaRepository.Delete(obj);
+        }
+
+    }
 
     }
 }
