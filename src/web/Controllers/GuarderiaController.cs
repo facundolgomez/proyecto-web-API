5e5e5f9d2407d4cc48607431159d9c154c05c894
FrancoBalduini <134542513+FrancoBalduini@users.noreply.github.com>
2024-06-12 23:42:40 -0300

    controller guarderia

---
+++ src/web/Controllers/GuarderiaController.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Interfaces;
+using Application.Models.Requests;
+using Application.Models;
+using Domain.Exceptions;
+using Domain.Entities;
+
+namespace web.Controllers 
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuarderiaController : ControllerBase 
+    {
+        private readonly IGuarderiaService _guarderiaService;
+        public GuarderiaController(IGuarderiaServicService guarderiaService)
+        {
+            _guarderiaService = guarderiaService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] GuarderiaCreateRequest guarderiaCreateRequest)
+        {
+            var newObj = _guarderiaService.Create(guarderiaCreateRequest);
+
+            return CreatedAtAction(nameof(Get), new { id = newObj.Id }, newObj);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] GuarderiaUpdateRequest guarderiaUpdateRequest)
+        {
+
+            try
+            {
+                _guarderiaService.Update(id, subjectUpdateRequest);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                _guarderiaService.Delete(id);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<GuarderiaDto>> GetAll()
+        {
+            return _guarderiaService.GetAll();
+        }
+
+
+
+        [HttpGet("[action]")]
+        public ActionResult<List<Guarderia>> GetAllFullData()
+        {
+            return _guarderiaService.GetAllFullData();
+        }
+
+
+
+        [HttpGet("{id}")]
+        public ActionResult<GuarderiaDto> Get([FromRoute] int id)
+        {
+            try
+            {
+                return _guarderiaService.GetById(id);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+
+}
